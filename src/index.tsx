96ae011ae0e3c462c9623c0bf10635628eb94baf
Simon Elvery <simon@elvery.net>
2021-06-08 11:48:07 +1000

    Implement dev-proxy

--- src/index.tsx
+++ src/index.tsx
@@ -10,95 +10,99 @@ import { Mark } from './constants';
 import { getMountValue, selectMounts } from '@abcnews/mount-utils';
 import Header from './components/Header';
 import ParallaxHeader from './components/ParallaxHeader';
-
-const PROJECT_NAME: string = 'carbon-bank';
-
-const align: 'right' = 'right';
+import { proxy } from '@abcnews/dev-proxy';
 
 type ScrollyData = { [key: string]: ScrollytellerDefinition<Mark> };
 
-const whenScrollytellersLoaded = new Promise<ScrollyData>((resolve, reject) => {
-  whenOdysseyLoaded.then(() => {
-    const scrollyDatas: ScrollyData = {};
-    selectMounts('scrollytellerNAME', { markAsUsed: false })
-      .map(mountEl => (getMountValue(mountEl).match(/NAME([a-z]+)/) || [])[1])
-      .filter(name => typeof name === 'string')
-      .forEach(name => {
-        try {
-          // Load scrollytellers
-          const scrollyData = loadScrollyteller<PanelData>(name, 'u-full');
+const PROJECT_NAME = 'carbon-bank';
+const align = 'right' as const;
 
-          // Convert panel data
-          const panels: PanelDefinition<Mark>[] = scrollyData.panels
-            .map(d => ({
-              ...d,
-              align: name !== 'header' ? align : undefined,
-              data: {
-                ...panelDataToMark(d.data),
-                standalone:
-                  d.nodes.length === 1 && d.nodes[0] instanceof Element && d.nodes[0].matches('.Quote') ? true : false
-              }
-            }))
-            .map((d, index, arr) => ({
-              ...d,
-              data: { ...d.data, next: arr[index + 1]?.data }
-            }));
+const init = () => {
+  const whenScrollytellersLoaded = new Promise<ScrollyData>((resolve, reject) => {
+    whenOdysseyLoaded.then(() => {
+      const scrollyDatas: ScrollyData = {};
+      selectMounts('scrollytellerNAME', { markAsUsed: false })
+        .map(mountEl => (getMountValue(mountEl).match(/NAME([a-z]+)/) || [])[1])
+        .filter(name => typeof name === 'string')
+        .forEach(name => {
+          try {
+            // Load scrollytellers
+            const scrollyData = loadScrollyteller<PanelData>(name, 'u-full');
 
-          // Replace the panel definiton on the global scrollyteller object
-          scrollyDatas[name] = { ...scrollyData, panels };
-        } catch (e) {
-          reject(e);
-        }
-        resolve(scrollyDatas);
-      });
+            // Convert panel data
+            const panels: PanelDefinition<Mark>[] = scrollyData.panels
+              .map(d => ({
+                ...d,
+                align: name !== 'header' ? align : undefined,
+                data: {
+                  ...panelDataToMark(d.data),
+                  standalone:
+                    d.nodes.length === 1 && d.nodes[0] instanceof Element && d.nodes[0].matches('.Quote') ? true : false
+                }
+              }))
+              .map((d, index, arr) => ({
+                ...d,
+                data: { ...d.data, next: arr[index + 1]?.data }
+              }));
+
+            // Replace the panel definiton on the global scrollyteller object
+            scrollyDatas[name] = { ...scrollyData, panels };
+          } catch (e) {
+            reject(e);
+          }
+          resolve(scrollyDatas);
+        });
+    });
   });
-});
 
-const renderIllustrations = () => {
-  const titleMount = selectMounts('cbillustration')[0];
+  const renderIllustrations = () => {
+    const titleMount = selectMounts('cbillustration')[0];
 
-  if (!titleMount) return;
+    if (!titleMount) return;
 
-  const parentEl = titleMount.parentElement;
+    const parentEl = titleMount.parentElement;
 
-  if (!parentEl) return;
+    if (!parentEl) return;
 
-  const titleEl = parentEl.querySelector('h1');
+    const titleEl = parentEl.querySelector('h1');
 
-  if (titleEl && titleEl.parentElement === parentEl) {
-    titleMount.removeAttribute('class');
-    parentEl.insertBefore(titleMount, titleEl);
-  }
+    if (titleEl && titleEl.parentElement === parentEl) {
+      titleMount.removeAttribute('class');
+      parentEl.insertBefore(titleMount, titleEl);
+    }
 
-  render(<Header />, titleMount);
-};
+    render(<Header />, titleMount);
+  };
 
-whenOdysseyLoaded.then(renderIllustrations);
-whenScrollytellersLoaded.then(renderApp);
-
-function renderApp(scrollyData: ScrollyData) {
-  for (let name in scrollyData) {
-    const { panels, mountNode, config } = scrollyData[name];
-    try {
-      if (name === 'header') {
-        render(<ParallaxHeader config={config} panels={panels} />, mountNode);
-      } else {
-        render(<App config={config} panels={panels} />, mountNode);
+  function renderApp(scrollyData: ScrollyData) {
+    for (const name in scrollyData) {
+      const { panels, mountNode, config } = scrollyData[name];
+      try {
+        if (name === 'header') {
+          render(<ParallaxHeader config={config} panels={panels} />, mountNode);
+        } else {
+          render(<App config={config} panels={panels} />, mountNode);
+        }
+      } catch (e) {
+        import('./components/ErrorBox').then(({ default: ErrorBox }) => {
+          render(<ErrorBox error={e} />, mountNode);
+        });
       }
-    } catch (e) {
-      import('./components/ErrorBox').then(({ default: ErrorBox }) => {
-        render(<ErrorBox error={e} />, mountNode);
-      });
     }
   }
-}
 
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    whenScrollytellersLoaded.then(renderApp);
-  });
-}
+  whenOdysseyLoaded.then(renderIllustrations);
+  whenScrollytellersLoaded.then(renderApp);
 
-if (process.env.NODE_ENV === 'development') {
-  console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
-}
+  if (module.hot) {
+    module.hot.accept('./components/App', () => {
+      whenScrollytellersLoaded.then(renderApp);
+    });
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    console.info(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
+  }
+};
+
+proxy(PROJECT_NAME).then(init);
